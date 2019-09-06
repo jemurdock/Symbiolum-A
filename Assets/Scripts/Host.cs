649e5d9aa654bcad9fc53c2d3b41f5d5d372cbf0
jemurdock <jessica.murdock@gmail.com>
2019-09-06 10:29:54 -0600

    tailgating

---
+++ Assets/Scripts/Host.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Host : MonoBehaviour
+{
+    public float speed = 1;
+    public float jumpHeight = 2;
+    public float size = 0;
+    public bool water = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
